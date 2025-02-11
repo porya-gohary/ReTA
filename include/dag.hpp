ef625ee98c85058cf765b7d444738ca33b178ee3
porya-gohary <gohary.porya@gmail.com>
2025-02-11 11:53:41 +0100

    performance improvement

--- include/dag.hpp
+++ include/dag.hpp
@@ -7,6 +7,7 @@
 #include <iostream>
 #include <fstream>
 #include <algorithm>
+#include <unordered_map>
 
 struct node {
 
@@ -38,7 +39,8 @@ struct edge {
 
 class dag {
 private:
-    std::vector<std::shared_ptr<node>> nodes;
+	// A map for nodes. The key is the node ID
+	std::unordered_map<unsigned long, std::shared_ptr<node>> nodes;
     std::vector<std::shared_ptr<edge>> edges;
     // a vector to keep ID of the leaves states
     std::vector<unsigned long> leaves;
@@ -49,15 +51,13 @@ public:
 
     void addNode(long parentId, unsigned long timeStamp, std::string label, std::string edgeLabel, std::string edgeQueue="") {
 		std::shared_ptr<node> n = std::make_shared<node>(numNodes, timeStamp, std::move(label));
-        nodes.push_back(n);
+		nodes.insert({numNodes, n});
         numNodes++;
         if (parentId != -1) {
             // find parent node
-            auto x = std::find_if(nodes.begin(), nodes.end(), [parentId](std::shared_ptr<node> const &tempNode) {
-                return tempNode->id == parentId;
-            });
+			auto x = nodes.find(parentId);
             if (x != nodes.end()) {
-                std::shared_ptr<node> parent = *x;
+                std::shared_ptr<node> parent = x->second;
                 // add parent to child
                 n->parents.push_back(parent->id);
                 // add child to parent
@@ -79,27 +79,17 @@ public:
         }
 
 
-        // sort nodes by timestamp
-        std::sort(nodes.begin(), nodes.end(),
-                  [](const std::shared_ptr<node> &a, const std::shared_ptr<node> b) {
-                      return a->timeStamp < b->timeStamp;
-                  });
-
     }
 
     void addEdge(long source, long destination, std::string edgeLabel) {
         // find source node
-        auto x = std::find_if(nodes.begin(), nodes.end(), [source](std::shared_ptr<node> const &tempNode) {
-            return tempNode->id == source;
-        });
+		auto x = nodes.find(source);
         if (x != nodes.end()) {
-            std::shared_ptr<node> sourceNode = *x;
+            std::shared_ptr<node> sourceNode = x->second;
             // find destination node
-            auto y = std::find_if(nodes.begin(), nodes.end(), [destination](std::shared_ptr<node> const &tempNode) {
-                return tempNode->id == destination;
-            });
+			auto y = nodes.find(destination);
             if (y != nodes.end()) {
-                std::shared_ptr<node> destinationNode = *y;
+                std::shared_ptr<node> destinationNode = y->second;
                 // add destination to source
                 sourceNode->children.push_back(destinationNode->id);
                 // add source to destination
@@ -127,17 +117,22 @@ public:
 		}), edges.end());
 
 		// if we don't need all the nodes, remove the none-leaf nodes from the node vector
-		nodes.erase(std::remove_if(nodes.begin(), nodes.end(), [](std::shared_ptr<node> const &tempNode) {
-			return !tempNode->children.empty();
-		}), nodes.end());
+		std::vector<unsigned long> nonLeafNodes;
+		for (const auto &n: nodes) {
+			if (std::find(leaves.begin(), leaves.end(), n.first) == leaves.end()) {
+				nonLeafNodes.push_back(n.first);
+			}
+		}
+		for(auto n: nonLeafNodes) {
+			nodes.erase(n);
+		}
+
 	}
 
     void updateNodeLabel(unsigned long id, std::string label) {
-        auto x = std::find_if(nodes.begin(), nodes.end(), [id](std::shared_ptr<node> const &tempNode) {
-            return tempNode->id == id;
-        });
+		auto x = nodes.find(id);
         if (x != nodes.end()) {
-            std::shared_ptr<node> n = *x;
+            std::shared_ptr<node> n = x->second;
             n->label = label;
         } else {
             log<LOG_CRITICAL>("Node in the DAG not found!");
@@ -152,9 +147,9 @@ public:
 
     unsigned long getLeafStateWithSmallestTimeStamp() {
         for (const auto &n: nodes) {
-            if (n->children.empty()) {
-                return n->id;
-            }
+			if (n.second->children.empty()) {
+				return n.second->id;
+			}
         }
         return -1;
     }
@@ -188,9 +183,9 @@ public:
         file << "\trankdir=LR;" << std::endl;
         file << "\tnode [fontname=Ubuntu]" << std::endl;
         file << "\tedge [fontname=Ubuntu,color=Red,fontcolor=Red]" << std::endl;
-        for (auto n: nodes) {
-            file << "\t" << n->id << " [label=\"" << n->label << "\"];" << std::endl;
-        }
+		for (const auto &n: nodes) {
+			file << "\t" << n.second->id << " [label=\"" << n.second->label << "\"];" << std::endl;
+		}
         for (auto e: edges) {
             file << "\t" << e->fromID << " -> " << e->toID << " [label=\"" << e->label << (e->queue)  << "\"];" << std::endl;
         }
